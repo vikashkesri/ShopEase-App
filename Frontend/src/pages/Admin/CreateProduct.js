e32c288623c4074cdbe3724debb288abbafc10a7
vikashkesri <vk9630623@gmail.com>
2025-10-03 23:17:31 +0530

    Update CreateProduct.js

--- Frontend/src/pages/Admin/CreateProduct.js
+++ Frontend/src/pages/Admin/CreateProduct.js
@@ -22,7 +22,7 @@ const CreateProduct = () => {
   const getAllCategory = async () => {
     try {
       const { data } = await axios.get(
-        "http://localhost:5000/api/v1/category/get-category"
+        "https://shopease-app-fvn8.onrender.com/api/v1/category/get-category"
       );
       if (data?.success) {
         setCategories(data.category);
@@ -59,7 +59,7 @@ const CreateProduct = () => {
       }
 
       const { data } = await axios.post(
-        "http://localhost:5000/api/v1/product/create-product",
+        "https://shopease-app-fvn8.onrender.com/api/v1/product/create-product",
         productData,
         {
           headers: {
