276bc17c363be12350fd28ea46c05dddb14a4713
vikashkesri <vk9630623@gmail.com>
2025-10-03 23:21:58 +0530

    Update ForgotPassword.js

--- Frontend/src/pages/Auth/ForgotPassword.js
+++ Frontend/src/pages/Auth/ForgotPassword.js
@@ -17,7 +17,7 @@ const ForgotPassword = () => {
     setLoading(true);
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/v1/auth/forgot-password", 
+        "https://shopease-app-fvn8.onrender.com/api/v1/auth/forgot-password", 
         { email, newPassword, answer },
         { headers: { "Content-Type": "application/json" } } 
       );
