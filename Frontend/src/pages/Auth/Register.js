64c43542245de6b1622c1eed61843ef85abee001
vikashkesri <vk9630623@gmail.com>
2025-10-03 23:23:24 +0530

    Update Register.js

--- Frontend/src/pages/Auth/Register.js
+++ Frontend/src/pages/Auth/Register.js
@@ -26,7 +26,7 @@ const Register = () => {
     try {
       setLoading(true);
       const res = await axios.post(
-        "http://localhost:5000/api/v1/auth/register", 
+        "https://shopease-app-fvn8.onrender.com/api/v1/auth/register", 
         {
           name,
           email,
