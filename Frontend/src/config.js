c7549bf31303d01f960ce25512e245f47417b9a5
vikashkesri <vk9630623@gmail.com>
2025-10-04 01:06:29 +0530

    Update config.js

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -3,7 +3,7 @@ let API;
 
 if (process.env.NODE_ENV === "development") {
   // local dev backend
-  API = "http://localhost:5000/api/v1";
+  API = "https://shopease-app-fvn8.onrender.com/api/v1";
 } else {
   // deployed backend
   API = "https://shopease-app-fvn8.onrender.com/api/v1";
