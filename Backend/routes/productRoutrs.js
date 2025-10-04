e509125dd1708fc4b063824fc3e1a9c76f89b93d
vikashkesri <vk9630623@gmail.com>
2025-10-05 01:54:01 +0530

    Update productRoutrs.js

--- Backend/routes/productRoutrs.js
+++ Backend/routes/productRoutrs.js
@@ -25,7 +25,8 @@ const router = express.Router();
 // PRODUCT ROUTES
 router.post("/create-product", requireSignIn, isAdmin, formidable(), createProductController);
 router.put("/update-product/:pid", requireSignIn, isAdmin, formidable(), updateProductController);
-router.get("/get-product", getProductController);
+router.get("/get-product/:id", getProductController);
+router.get("/get-product/:id", getProductController);
 router.get("/get-product/:slug", getSingleProductController);
 router.get("/product-photo/:pid", productPhotoController);
 router.delete("/delete-product/:pid", requireSignIn, isAdmin, deleteProductController);
@@ -36,7 +37,6 @@ router.get("/search", searchProductController);
 router.get("/product-category/:slug", productCategoryController);
 router.get("/related-product/:pid/:cid", relatedProductController);
 router.get("/get-products", getAllProductsController);
-
 
 
 // Create Order after payment
